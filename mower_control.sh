8caf841b80dd266ebe4970b0f8ab1363c5294438
DaKarlii <61060832+DaKarlii@users.noreply.github.com>
2023-05-21 19:02:31 +0200

    Update mower_control.sh
    Update after forke

--- mower_control.sh
+++ mower_control.sh
@@ -1,1 +1,2 @@
-/usr/local/bin/python3.10 /PATH/gardena/mower_control.py $1 $2
+#!/bin/bash
+/usr/local/bin/python3.10 /PATH/gardena/mower_control.py $1 $2 #Update Path for Script and Python (Version)
